3125f06c66e4bffc1c6eb86716a52d2487429e7a
Chris Martin <bmg.martin08@gmail.com>
2020-11-03 05:04:19 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,5 +30,6 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
-  matrix[row][column] = new_value[matrix.to_a]
+  matrix[row][column] = new_value
+  puts new_value
 end
